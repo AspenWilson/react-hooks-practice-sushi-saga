add9618820b67cda5f5c30e5e6f32db6a5741d8f
Aspen Wilson <aspen.schell@gmail.com>
2023-04-19 17:25:01 -0700

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+
+  const [sushis, setSushis] = useState([])
+  const [sushiBelt, setSushiBelt] = useState(0)
+  const [budget, setBudget] = useState(200)
+  const displayCount = 4
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/sushis`)
+    .then((resp) => resp.json())
+    .then((sushis) => setSushis(sushis))
+  }, [])
+
+  function setEaten(piece){
+
+    const remainingBudget = budget - piece.price
+
+    if (remainingBudget >= 0) {
+    setBudget(remainingBudget)
+
+    setSushis(
+      sushis.map((sushi) => 
+      sushi.id===piece.id ? {...sushi, eaten: true } : sushi
+    )
+    )
+  }
+}
+
+  function handleMoreBtn () {
+    setSushiBelt((sushiBelt + displayCount) % sushis.length)
+  }
+
+
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer  sushis={sushis.slice(sushiBelt, sushiBelt + displayCount)} handleMore={handleMoreBtn} handleEatSushi={setEaten}/>
+      <Table budget={budget} plates={sushis.filter(sushi => sushi.eaten)}/>
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,17 @@
 import React from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({sushis, handleMore, handleEatSushi}) {
+
+  const allSushi=sushis.map((sushi) => {
+    return <Sushi key ={sushi.id} sushi={sushi} handleEatSushi={handleEatSushi}/>
+  })
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {allSushi}
+      <MoreButton handleMore={handleMore}/>
     </div>
   );
 }
